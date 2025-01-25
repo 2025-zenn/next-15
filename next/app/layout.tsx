d1f6a4f8daf6e12c2a9b1c48c92b84f714e677c9
iniwa-takaya <i.iniwa.daitokuji@gmail.com>
2025-01-25 16:51:26 +0900

    ディレクトリー整理

--- next/app/layout.tsx
+++ next/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navigation from "@/app/_components/Navigation"
+import Navigation from "@/_components/Navigation"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
